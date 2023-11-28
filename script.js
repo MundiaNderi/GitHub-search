44c563660c2535334f5a197082937a5da6998f32
Serah Nderi <sarahngima77@gmail.com>
2023-11-28 20:34:38 +0300

    Toggle, dark and Light mode

--- script.js
+++ script.js
@@ -2,20 +2,36 @@ const themeToggle = document.getElementById('themeToggle');
 const themeText = document.getElementById('themeText');
 const moonIcon = document.getElementById('moonIcon');
 const themeIcon = document.getElementById('themeIcon');
+const sunIcon = document.getElementById('sunIcon');
 
+themeToggle.addEventListener('click', toggleTheme);
+themeToggle.addEventListener('touchstart', toggleTheme);
 
-themeToggle.addEventListener('click', () => {
+function toggleTheme(event) {
+    event.preventDefault();
+
     const body = document.querySelector('body');
     body.classList.toggle('dark-mode');
 
     if (body.classList.contains('dark-mode')) {
-        themeText.textContent = 'Light';
-        themeIcon.style.display = '#141D2F'; // Change color to a darker shade
-        moonIcon.setAttribute('d', 'images/002-sun.svg'); // Change color to a darker shade
+        // Show sun, hide moon
+        sunIcon.style.display = 'inline-block';
+        moonIcon.style.display = 'none';
+        
+        // Change background color to dark
+        body.style.backgroundColor = '#141D2F';
+        statistics.style.backgroundColor = '#141D2F';
+        search.style.backgroundColor = '#141D2F'; // Darker color for search
+        octocat.style.backgroundColor = '#14142F';
     } else {
-        themeText.textContent = 'Dark';
-        themeIcon.style.fill = '#141D2F';
-        moonIcon.setAttribute('D', 'images/moon.svg') 
-        // Replace the sun SVG with the moon SVG 
+        // Show moon, hide sun
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline-block';
+        
+        // Reset background color
+        body.style.backgroundColor = '#FEFEFE';
+        statistics.style.backgroundColor = '#FEFEFE'; 
+        search.style.backgroundColor = '#FEFEFE'; // Reset search background
+        octocat.style.backgroundColor = '#FEFEFE'
     }
-}) 
+}
