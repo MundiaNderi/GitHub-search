4a6ce04e3b4133279d75cfa3a51f54426ca2ca7a
Serah Nderi <sarahngima77@gmail.com>
2023-12-01 01:59:44 +0300

    API changes

--- script.js
+++ script.js
@@ -39,68 +39,79 @@ function toggleTheme(event) {
 // Github Users API
 // Get the search box 
 document.addEventListener('DOMContentLoaded', function() {
-    let searchBox = document.getElementById('search-box');
-    let searchText = document.getElementById('search-text')
+    const searchBox = document.getElementById('search-box');
     
-    // Add an event listener on the input field
-    searchBox.addEventListener('click', function(e) {
+    searchBox.addEventListener('click', async function(e) {
         e.preventDefault();
 
-        // Access the input value when the button is clicked
-        let userInput = searchText.value;
+        const searchText = document.getElementById('search-text').value.trim();
+        if (searchText !== '') {
+            try {
+                const originalName = searchText.split(' ').join('');
+                const userData = await fetchUserData(originalName);
+                updateUserProfile(userData);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        } else {
+            console.error('Empty search field');
+        }
+    });
+});
 
-        var originalName = userInput.split(' ').join('')
-        alert(originalName)
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    
+    const monthNames = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+    
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const year = date.getFullYear();
 
-        fetch("https://api.github.com/users/"+originalName)
-        .then((result) => result.json())
-        .then((data) => {
-            console.log(data);
-        })
-
-    })
-})
-
-
-
-
-// Feed the data to the html page 
-
-const userData = {
-    avatar_url : "https://avatars.githubusercontent.com/u/98798719?v=4",
-    name: "Serah Nderi",
-    created_at : "2022-02-01T05:53:55Z",
-    login: "MundiaNderi",
-    bio: "Junior Software Engineer",
-    public_repos: "37",
-    followers: '13',
-    following: '23',
-    location: 'Nairobi',
-    twitter_username: "nderisarah",
-    ulr: "",
-    github: "",
-
+    return `Joined ${day} ${monthNames[monthIndex]} ${year}`;
 }
 
-// Access HTML Elements by their IDs
-const avatar = document.getElementById('avatar');
-const name = document.getElementById('name');
-const date = document.getElementById('created');
-const handle = document.getElementById('handle');
-const bio = document.getElementById('bio');
-const repo = document.getElementById('repo');
-const follow = document.getElementById('follow')
-const following = document.getElementById('following')
+// format "YYYY-MM-DDTHH:MM:SSZ"
+const joinedDate = formatDate(userData.created_at);
+console.log(joinedDate); // Output: Joined 25 Jan 2011
 
 
-// Populate HTML elements with the user data
-avatar.src = userData.avatar_url;
-name.textContent = userData.name;
-date.textContent = userData.created_at;
-handle.textContent = '@' + userData.login;
-bio.textContent = userData.bio;
-repo.textContent = userData.public_repos;
-follow.textContent = userData.followers;
-following.textContent = userData.following;
-city.textContent = userData.location
-twitter.textContent = userData.twitter_username;
+async function fetchUserData(username) {
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        if (!response.ok) {
+            throw new Error('User not found');
+        }
+        return await response.json();
+    } catch (error) {
+        throw new Error('Error fetching user data:', error);
+    }
+}
+
+function updateUserProfile(userData) {
+    const avatar = document.getElementById('avatar');
+    const name = document.getElementById('name');
+    const date = document.getElementById('created');
+    const handle = document.getElementById('handle');
+    const bio = document.getElementById('bio');
+    const repo = document.getElementById('repo');
+    const follow = document.getElementById('follow');
+    const following = document.getElementById('following');
+    const city = document.getElementById('city');
+    const twitter = document.getElementById('twitter');
+
+    // Populate HTML elements with the retrieved user data
+    avatar.src = userData.avatar_url;
+    name.textContent = userData.name;
+    date.textContent = formatDate(userData.created_at);
+    handle.textContent = `@${userData.login}`;
+    bio.textContent = userData.bio;
+    repo.textContent = userData.public_repos;
+    follow.textContent = userData.followers;
+    following.textContent = userData.following;
+    city.textContent = userData.location;
+    twitter.textContent = userData.twitter_username;
+}
